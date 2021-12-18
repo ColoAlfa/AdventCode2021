de82f05627158d51fc544e98bbcc3de3a45a70f6
Colodidac <didaccolominas@gmail.com>
2021-12-18 20:42:28 +0100

    Second Part Day3

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -6,7 +6,7 @@ def read():
     numberList = []
     for number in clearData:
         numberList.append(list(number))
-    print(numberList)
+    #print(numberList)
     return numberList
 
 
@@ -48,5 +48,84 @@ def problemA():
     print('Valor de gamma: ', gammaRate, 'valor de epsilon: ',
           epsilonRate, '\n Total PowerConsumption: ', powerConsumption)
 
+def problemB():
+    
+    """"
+    Tenemos que detectar el valor de bit mas común, una vez lo tenemos nos quedaremos
+    con los numeros que tengan ese valor en ese mismo bit, y así hasta que solo quede uno.
 
-problemA()
+    OXYGEN GENERATOR RATING
+    """
+    #Variables Zone:
+    data=read()
+    count1=0
+    count0=0
+    oxygenGeneratorRating=[]
+    CO2ScrubberRating=[]
+    valor=11
+    oxygenVal=0
+    CO2Val=0
+
+    #Maximum
+    for index in range(len(data[0])):
+        for number in data:
+            if number[index]=='1':
+                count1+=1
+            else:
+                count0+=1
+        if count1>=count0:
+            for number in data:
+                if number[index]=='1':
+                    oxygenGeneratorRating.append(number)
+        else:
+            for number in data:
+                if number[index]=='0':
+                    oxygenGeneratorRating.append(number)
+        data=oxygenGeneratorRating
+        oxygenGeneratorRating=[]
+        count0=0
+        count1=0
+    print("The Oxygen Generator Rating is: ",data)
+    oxygenGeneratorRating=data
+    data=read()
+    #print(data)
+    #Minimun
+    for index in range(len(data[0])):
+        if(len(data)==1):#Aquí nos aparece un error que antes no debido  aque cuando solo quede uno
+            break        #El valor que queremos sera ese uno que queda no el que tenga menos pork no hay. 
+        for number in data:
+            if number[index]=='1':
+                count1+=1
+            else:
+                count0+=1
+        
+        if count1<count0:
+            for number in data:
+                if number[index]=='1':
+                    CO2ScrubberRating.append(number)
+        else:
+            for number in data:
+                if number[index]=='0':
+                    CO2ScrubberRating.append(number)
+        
+        #print("loop:", index, "value",data)
+        data=CO2ScrubberRating
+        CO2ScrubberRating=[]
+        count0=0
+        count1=0
+        
+    CO2ScrubberRating=data  
+    print(CO2ScrubberRating)
+    print(oxygenGeneratorRating) 
+
+    for index in range(len(CO2ScrubberRating[0])):
+        oxygenVal=oxygenVal+int(oxygenGeneratorRating[0][index])*2**valor
+        CO2Val=CO2Val+int(CO2ScrubberRating[0][index])*2**valor
+        valor-=1
+    
+    print("The CO2 Scrubber Rating is: ",CO2Val, "\n the Oxygen Generator Rating is: ", oxygenVal)
+    result=oxygenVal*CO2Val
+    print("The result is: ",result)
+problemB()
+
+
